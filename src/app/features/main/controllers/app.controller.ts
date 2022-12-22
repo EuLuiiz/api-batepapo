78d468d0bb21398a408c37feede41e8d52ed255d
EuLuiiz <victorluiz011@gmail.com>
2022-12-22 03:49:29 -0300

    feat current by token

--- src/app/features/main/controllers/app.controller.ts
+++ src/app/features/main/controllers/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
+import { CurrentUser } from '../../auth/decorators/currentUser.decorator';
+import { IsPublic } from '../../auth/decorators/isPublic.decorator';
+import { User } from '../../users/interfaces/user.entity';
 import { AppService } from '../services/app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
+  @IsPublic()
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('me')
+  getMe(@CurrentUser() user: User): string {
+    return `O usuário retirado do Token JWT é: ${user.nickname} e seu ID é: '${user.id}'`
   }
 }
